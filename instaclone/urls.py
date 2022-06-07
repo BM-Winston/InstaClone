564e233b372eacd8147c7cbb9cc99e48802ab7e0
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-07 13:59:02 +0300

    update app urls

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -1,6 +1,10 @@
+from django.conf import settings
 from django.conf.urls import url
 from . import views
 
 urlpatterns=[
     url('^$',views.hello,name = 'hello'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
