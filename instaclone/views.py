8c7f9c4ff721100fdf8352ac3a2186ec2324c386
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-07 12:21:03 +0300

    Add content in html file

--- instaclone/views.py
+++ instaclone/views.py
@@ -5,4 +5,4 @@ from django.http  import HttpResponse
 
 # Create your views here.
 def hello(request):
-    return HttpResponse('Hello there! This is InstaClone.Enjoy')
+    return render(request, 'index.html')
