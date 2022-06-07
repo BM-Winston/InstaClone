585a3e3c4408f8b2f0d3d53ff83624ab0602568e
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-07 13:42:18 +0300

    make migrations for models

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    # image = models.ImageField(upload_to='images/')
+    image_name = models.CharField(max_length=30)
+    image_caption = models.TextField()
+    likes = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
+    
+    
+
+    def __str__(self):
+        return self.image_name
+
+class Profile(models.Model):
+    # profile_photo = models.ImageField('profile_photo',blank=True)
+    profile_bio = models.TextField()
+    
+
+    def __str__(self):
+        return self.user.username
