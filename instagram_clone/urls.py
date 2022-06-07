1b126cdd33b47aee35a2efc41ac57b00ff84d1b7
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-07 13:48:24 +0300

    change urls of project

--- instagram_clone/urls.py
+++ instagram_clone/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^instaclone/',include('instaclone.urls'))
+    url(r'',include('instaclone.urls'))
 ]
 
