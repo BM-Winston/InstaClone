4f35700c87161aae595260c05ef62376b1db8be9
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-07 13:56:56 +0300

    add imagefiled in models

--- instaclone/models.py
+++ instaclone/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Image(models.Model):
-    # image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/')
     image_name = models.CharField(max_length=30)
     image_caption = models.TextField()
     likes = models.IntegerField(default=0)
@@ -15,7 +15,7 @@ class Image(models.Model):
         return self.image_name
 
 class Profile(models.Model):
-    # profile_photo = models.ImageField('profile_photo',blank=True)
+    profile_photo = models.ImageField('profile_photo',blank=True)
     profile_bio = models.TextField()
     
 
