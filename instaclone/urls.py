cfa472b57382d0438f876fda0b0684a4fe01dcf6
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-10 00:49:48 +0300

    Update navbar

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -5,8 +5,8 @@ from . import views
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.images,name = 'images'),
-    url('signup/', views.signup, name='signup'),
+    url('signup/',views.images,name = 'images'),
+    url('^$', views.signup, name='signup'),
     url('login/', views.login, name='login'),
     url('logout/', views.logout, name='logout'),
     url('account/', include('django.contrib.auth.urls')), 
